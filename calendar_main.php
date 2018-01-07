9f458f8f7bd68d2bef2c044e35cd2edede574e4a
J Kenney <kenney@moboard.com>
2018-01-06 22:17:03 -0500

    Updated Priorities

--- calendar_main.php
+++ calendar_main.php
@@ -278,12 +278,14 @@
               $sspec = $l0."_".$counter."/".$cate;
             } elseif (isset($access[$l0."/".$cate])) {
               $sspec = $l0."/".$cate;
-            } elseif (isset($access["*/".$cate])) {
-              $sspec = "*/".$cate;
             } elseif (isset($access[$l0."_".$counter])) {
               $sspec = $l0."_".$counter;
+            } elseif (isset($access[$l0."/*"])) {
+              $sspec = $l0."/*";
             } elseif (isset($access[$l0."_".$counter."/all"])) {
               $sspec = $l0."_".$counter."/all";
+            } elseif (isset($access["*/".$cate])) {
+              $sspec = "*/".$cate;
             } elseif (isset($access[$l0])) {
               $sspec = $l0;
             } elseif (isset($access["*"])) {
