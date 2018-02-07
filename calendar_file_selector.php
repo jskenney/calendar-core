381857491f210c75c6494e5430b8c01107bc2d61
J. Kenney <kenney@usna.edu>
2018-02-07 12:56:20 -0500

    Updated file view to allow editing of just HTML files

--- calendar_file_selector.php
+++ calendar_file_selector.php
@@ -4,7 +4,7 @@
     echo "<table class='table table-striped table-bordered'>";
     echo "<thead>";
     echo "";
-    echo "<tr><th rowspan=2>Type</th><th rowspan=2>Category</th><th rowspan=2>File</th><th colspan=5>Security</th><th rowspan=2>View / Edit</th></tr>";
+    echo "<tr><th rowspan=2>Type</th><th rowspan=2>Category</th><th rowspan=2>File</th><th colspan=5>Security</th><th rowspan=2>View / Edit<br>HTML</th></tr>";
     echo "<tr><th>Visible</th><th>Month</th><th>Day</th><th>Year</th><th>Dynamic</th></tr>";
     echo "</thead>";
     echo "<tbody>";
@@ -25,7 +25,12 @@
       echo "<td>".$value['year']."</td>";
       echo "<td>".$value['dynamic']."</td>";
       $link .='&edit=1';
-      echo "<td><a href='$link'>view</a></td>";
+      $ext = pathinfo($fn, PATHINFO_EXTENSION);
+      if ($ext == 'htm' || $ext == 'html') {
+        echo "<td><a href='$link'>view</a></td>";
+      } else {
+        echo "<td></td>";
+      }
       echo "</tr>";
     }
     echo "</table>";
